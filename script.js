b955c1b3bb3aa5f0606f858117d43b703496f141
yusufalhelou <108984728+yusufalhelou@users.noreply.github.com>
2023-09-15 23:45:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,14 @@
-// Define a function that creates a hidden input element and pastes the clipboard data into it
-function pasteClipboardData() {
-  // Create a hidden input element
-  var hiddenInput = document.createElement("input");
-  // Set its type to text
-  hiddenInput.type = "text";
-  // Set its style to hide it from the view
-  hiddenInput.style = "position: absolute; left: -1000px; top: -1000px";
-  // Append it to the body
-  document.body.appendChild(hiddenInput);
-  // Focus on it
-  hiddenInput.focus();
-  // Execute the paste command
-  document.execCommand("paste");
-  // Get the clipboard data from the hidden input element
-  var clipboardData = hiddenInput.value;
-  // Set the value of the input element with id="clipboard-data" to the clipboard data
-  document.getElementById("clipboard-data").value = clipboardData;
-  // Return the hidden input element
-  return hiddenInput;
-}
-
-// Define a function that takes the URL from the hidden input element and modifies it
-function modifyURLFromHiddenInput() {
+// Define a function that reads the URL from the clipboard and modifies it
+async function modifyURLFromClipboard() {
   try {
-    // Paste the clipboard data into the hidden input element
-    var hiddenInput = pasteClipboardData();
-    // Get the value of the input element, which is the URL from the clipboard
-    var url = hiddenInput.value;
+    // Read the URL from the clipboard
+    var url = await navigator.clipboard.readText();
     // Use a regular expression to find the input in the URL
     var input = url.match(/150px\/(.+)\.jpeg/)[1];
     // Construct the new URL based on the input
     var new_url = "https://flim-1-0-2.s3.eu-central-1.amazonaws.com/full/" + input + ".png";
     // Display the new URL in a paragraph element with id="new-url"
     document.getElementById("new-url").innerHTML = new_url;
-    // Remove the hidden input element from the body
-    document.body.removeChild(hiddenInput);
   } catch (error) {
     console.error(error.message);
   }
@@ -47,3 +21,20 @@ function openNewTabWithModifiedURL() {
   // Open the new URL in a new tab
   window.open(new_url, "_blank");
 }
+
+// Add a button that calls the modifyURLFromClipboard function when clicked
+var button = document.createElement("button");
+button.textContent = "Modify URL From Clipboard";
+button.addEventListener("click", modifyURLFromClipboard);
+document.body.appendChild(button);
+
+// Add another button that calls the openNewTabWithModifiedURL function when clicked
+var button2 = document.createElement("button");
+button2.textContent = "Open New Tab With Modified URL";
+button2.addEventListener("click", openNewTabWithModifiedURL);
+document.body.appendChild(button2);
+
+// Add a paragraph element that will display the modified URL
+var p = document.createElement("p");
+p.id = "new-url";
+document.body.appendChild(p);
